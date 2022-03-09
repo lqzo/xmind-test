efb7adac87d0aa80af85fc2dfc6ea1c49532b9d9
李轻舟 <18903968616@163.com>
2022-03-09 13:28:24 +0800

    Add ConfigProvider

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ElConfigProvider } from "element-plus";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+
 import Cashbook from "./components/Cashbook.vue";
 </script>
 
 <template>
-  <Cashbook />
+  <el-config-provider :locale="zhCn">
+    <Cashbook />
+  </el-config-provider>
 </template>
 
 <style>
