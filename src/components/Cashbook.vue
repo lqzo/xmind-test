4ac457748c6ff589b0b67b5f1af5186146a9e7c4
李轻舟 <18903968616@163.com>
2022-03-09 13:08:06 +0800

    优化数据表格的展示效果

--- src/components/Cashbook.vue
+++ src/components/Cashbook.vue
@@ -1,11 +1,46 @@
 <script setup>
 import { defineComponent, ref } from "vue";
+import { formatDate } from "../utils";
 import { ElTable } from "element-plus";
 
 defineComponent({
   "el-table": ElTable,
 });
 
+// key name mapping
+const keyNameMap = {
+  id: "ID",
+  time: "账单时间",
+  type: "账单类型",
+  amount: "账单金额",
+  category: "账单分类",
+  name: "分类名称",
+};
+
+// import categories data
+const categories = ref([]);
+fetch("src/assets/file/categories.csv", {
+  headers: {
+    "Content-Type": "text/csv",
+  },
+}).then((res) => {
+  res.text().then((text) => {
+    const data = text.split("\n").map((row) => {
+      return row.split(",");
+    });
+    const keys = data[0];
+    const values = data.slice(1);
+    const result = values.map((row) => {
+      return keys.reduce((obj, key, index) => {
+        obj[key] = row[index];
+        return obj;
+      }, {});
+    });
+    categories.value = result;
+  });
+});
+
+// import bill data
 const originData = ref([]);
 const columns = ref([]);
 fetch("src/assets/file/bill.csv", {
@@ -20,7 +55,7 @@ fetch("src/assets/file/bill.csv", {
     const keys = data[0];
     columns.value = keys.map((key) => {
       return {
-        title: key,
+        title: keyNameMap[key],
         key,
       };
     });
@@ -34,6 +69,31 @@ fetch("src/assets/file/bill.csv", {
     originData.value = result;
   });
 });
+
+/**
+ * show bill data
+ * @param {String} key
+ * @param {String} val
+ * @return {String}
+ */
+function showBilldata(key, val) {
+  if (key === "category") {
+    const category = categories.value.find((item) => {
+      return item.id === val;
+    });
+    return category ? category.name : "";
+  }
+  if (key === "type") {
+    return val === "1" ? "收入" : "支出";
+  }
+  if (key === "amount") {
+    return `${val} 元`;
+  }
+  if (key === "time") {
+    return formatDate(+val);
+  }
+  return val;
+}
 </script>
 
 <template>
@@ -45,7 +105,11 @@ fetch("src/assets/file/bill.csv", {
       :label="column.title"
       align="center"
       header-align="center"
-    />
+    >
+      <template #="{ row }">
+        {{ showBilldata(column.key, row[column.key]) }}
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
